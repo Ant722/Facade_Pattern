1e79cc2c39c4bb6ec94ca7d57a35bd9b3e18bfc2
Ant722 <Ant722@mail.ru>
2023-05-03 22:10:34 +0300

    Программа готова

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("1011","1011"));
+        System.out.println(bins.sum("10101","10000001"));
+        System.out.println(bins.sum("1011","10101"));
     }
 }
